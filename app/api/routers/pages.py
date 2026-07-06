1b98b9769c37e71d0102a864833fee52ed709abe
gitmskhl <gglmskhl@gmail.com>
2026-07-06 12:22:05 +0300

    include in schema = False for step page

--- app/api/routers/pages.py
+++ app/api/routers/pages.py
@@ -77,7 +77,7 @@ async def admin_step_editor_page(request: Request, step_id: int):
     )
 
 
-@router.get("/{course_slug}/steps/{step_id}", response_class=HTMLResponse)
+@router.get("/{course_slug}/steps/{step_id}", response_class=HTMLResponse, include_in_schema=False)
 async def step_viewer_page(
     request: Request,
     course_slug: str,
