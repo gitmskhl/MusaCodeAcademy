850c12099be7c11d74a5c41f4f99e3a609b1f766
gitmskhl <gglmskhl@gmail.com>
2026-07-12 16:43:22 +0300

    Check worker

--- worker/main.py
+++ worker/main.py
@@ -1,12 +1,16 @@
 import asyncio
 
 from app.core.redis import redis
-
+from app.queue.submission import dequeue
 
 async def main():
-    pong = await redis.ping()
-    print(pong)
+    await redis.ping()
+    print('Worker started')
 
+    while True:
+        print('Waiting for submission...')
+        submission_id = await dequeue()
+        print(submission_id)
 
 if __name__ == "__main__":
     asyncio.run(main())
