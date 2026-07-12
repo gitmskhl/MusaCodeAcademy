be0c5feccc0ec9402a6a8b3518a0ee5cfb8d85ea
gitmskhl <gglmskhl@gmail.com>
2026-07-12 16:53:49 +0300

    Add socket_timeout=None

--- app/core/redis.py
+++ app/core/redis.py
@@ -5,5 +5,6 @@ from app.core.config import settings
 redis = Redis.from_url(
     url=settings.redis_url,
     encoding="utf-8",
-    decode_responses=True
+    decode_responses=True,
+    socket_timeout=None
 )
